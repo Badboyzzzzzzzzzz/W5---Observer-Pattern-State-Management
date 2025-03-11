64a0bb3c0a8c1629588cdf3ef3df4fe2d73c6351
socheat <socheatcheng143@gmail.com>
2025-03-11 21:32:19 +0700

    Update

--- lib/EXERCISE-2/screen/home.dart
+++ lib/EXERCISE-2/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:observe_statemanagement/EXERCISE-2/provider/count_notifier.dart';
+import 'package:observe_statemanagement/EXERCISE-2/screen/color_tap.dart';
 import 'package:observe_statemanagement/EXERCISE-2/screen/statistice.dart';
 import 'package:provider/provider.dart';
 
@@ -44,74 +45,3 @@ class _HomeState extends State<Home> {
   }
 }
 
-class ColorTapsScreen extends StatefulWidget {
-  const ColorTapsScreen({super.key});
-  @override
-  ColorTapsScreenState createState() => ColorTapsScreenState();
-}
-
-class ColorTapsScreenState extends State<ColorTapsScreen> {
-  @override
-  Widget build(BuildContext context) {
-    print('Screen Color Tap');
-    return Scaffold(
-      appBar: AppBar(title: Text('Color Taps')),
-      body: Column(
-        children: [
-          ColorTap(
-            type: CardType.red,
-          ),
-          ColorTap(
-            type: CardType.blue,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ColorTap extends StatelessWidget {
-  final CardType type;
-
-  const ColorTap({
-    super.key,
-    required this.type,
-  });
-
-  Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
-
-  @override
-  Widget build(BuildContext context) {
-    print('');
-    return Consumer<ColorCounter>(
-      builder: (context, count, child) {
-        final countTap = type == CardType.red ? count.redCount : count.blueCount;
-        print('${type == CardType.red ? 'red' : 'blue'} tap with count: $countTap');
-        return GestureDetector(
-          onTap: () {
-            if (type == CardType.red) {
-              count.incrementCountRed();
-            } else {
-              count.incrementCountBlue();
-            }
-          },
-          child: Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: backgroundColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            width: double.infinity,
-            height: 100,
-            child: Center(
-              child: Text(
-                'Taps: $countTap',
-                style: TextStyle(fontSize: 24, color: Colors.white),
-              ),
-            )
-          ),
-        );
-      },
-    );
-  }
-}
